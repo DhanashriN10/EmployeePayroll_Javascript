9756c701d87342026f551b4966e4a2b476846dfd
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-05-29 22:53:23 +0530

    Calculating no. of working days or total hours per month is reached

--- EmployeePayroll_Day38.js
+++ EmployeePayroll_Day38.js
@@ -1,31 +1,37 @@
-//UC4:-- Calculating Wages for a Month
+//UC5:-- Calculating Wages till Number of Working Days or Total Working Hours per month is Reached
 
- const IS_PART_TIME = 1;  
- const IS_FULL_TIME = 2; 
- const PART_TIME_HOURS = 4; 
- const FULL_TIME_HOURS = 8; 
- const WAGE_PER_HOUR = 20; 
- const NUM_OF_WORKING_DAYS = 2;
- function getWorkingHours (empCheck)
+
+const IS_PART_TIME = 1;  
+const IS_FULL_TIME = 2; 
+const PART_TIME_HOURS = 4; 
+const FULL_TIME_HOURS = 8; 
+const WAGE_PER_HOUR = 20; 
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+
+function getWorkingHours (empCheck)
  {
-     switch(empCheck)
-   {
-         case IS_PART_TIME:
-               return PART_TIME_HOURS;
-         case IS_FULL_TIME:
-               return FULL_TIME_HOURS;
-         default:
-               return 0;
-   }
+         switch(empCheck)
+		{
+    		case IS_PART_TIME:
+    				return PART_TIME_HOURS;
+    		case IS_FULL_TIME:
+     				return FULL_TIME_HOURS;
+    		default:
+     				return 0;
+		}
  }
+let empHours = 0;
+let totalWorkingDays = 0;
+let totalWorkingHours = 0;
+let totalEmpHrs=0;
 
-
-let empHrs=0;
-for (let day=0;day<NUM_OF_WORKING_DAYS;day++)
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 {
+	totalWorkingDays++;
 	let empCheck=Math.floor(Math.random()*10)%3;
-	empHrs+=getWorkingHours(empCheck);
+	totalEmpHrs+=getWorkingHours(empCheck);
 }
 
-let empWage=empHrs*WAGE_PER_HOUR;
-console.log("Total Hrs:",+empHrs+"Emp Wage:"+empWage);
+let empWage=totalEmpHrs*WAGE_PER_HOUR;
+console.log("UC5-Total Days:"+totalWorkingDays+"Total Hrs:"+totalEmpHrs+"Emp Wage:"+empWage);
