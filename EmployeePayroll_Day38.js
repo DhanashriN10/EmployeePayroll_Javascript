afc312d734244a7eb82040082e9b4a7c66d6bbd5
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-06-01 00:33:32 +0530

    UC13:Use RegEx for Name and Throw Error

--- EmployeePayroll_Day38.js
+++ EmployeePayroll_Day38.js
@@ -1,6 +1,8 @@
-//bility to extendEmployee Payroll Data to store gender and start date
+/*Ability to check the name
+starts with capital and has at
+least 3 characters - Use Regex Pattern - Use Try Catch in case of Error*/
 
-//UC12:-- Extend Employee Payroll Class
+//UC13:-- RegEx for Name and throw Error
 
 class EmployeePayrollData
 {
@@ -24,21 +26,34 @@ class EmployeePayrollData
 }
 set name(name)
 {
-	this._name=name;
+	let nameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$');
+	if (nameRegex.test(name))
+		this._name=name;
+	else
+		throw 'Name is Incorrect!';
 }
+
 toString()
 {
+
 	const options={year:'numeric',month:'long',day:'numeric'};
 	const empDate=this.startDate===undefined ? "undefined": this.startDate.toLocaleDateString("en-US",options);
 
 	return "id="+this.id+",name="+this.name+"salary="+this.salary+", "+"gender="+this.gender+",startDate="+empDate;
-}
 
 }
+}
 
-let employeePayrollData=new EmployeePayrollData(1,"Mark",30000);
+let employeePayrollData= new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
-employeePayrollData.name="john";
-console.log(employeePayrollData.toString());
-let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
+try
+{
+	employeePayrollData.name="john";
+	console.log(employeePayrollData.toString());
+}
+catch(e)
+{
+	console.error(e);
+}
+let newEmployeePayrollData= new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
 console.log(newEmployeePayrollData.toString());
