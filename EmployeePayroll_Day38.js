01563ea4e46f95b6f48993d0e9b4c7768a08e0cf
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-05-31 22:56:41 +0530

    UC9:Daily Wage and Daily Hour Map

--- EmployeePayroll_Day38.js
+++ EmployeePayroll_Day38.js
@@ -1,54 +1,33 @@
-/*Store the Day and theDaily Wage along
-with the Total Wage - Use Map to store Day wise Wage the map*/
-const IS_PART_TIME = 1;  
-const IS_FULL_TIME = 2; 
-const PART_TIME_HOURS = 4; 
-const FULL_TIME_HOURS = 8; 
-const WAGE_PER_HOUR = 20; 
+/*UC 9:--Use the Daily Wage Map and
+Daily Hour Map perform
+following operations using
+Arrow Functions
+a. Calc total Wage and total hours worked
+b. Show the full workings days, part working days and
+no working days*/
 
+//UC 9:-- Arrow Function
 
-function getWorkingHours (empCheck)
- {
-         switch(empCheck)
-		{
-    		case IS_PART_TIME:
-    				return PART_TIME_HOURS;
-    		case IS_FULL_TIME:
-     				return FULL_TIME_HOURS;
-    		default:
-     				return 0;
-		}
- }
-
-
-//UC 6 Arrays and 7 Maps
-
-const MAX_HRS_IN_MONTH=160;
-const NUM_OF_WORKING_DAYS=20;
-let totalEmpHrs=0;
-let totalWorkingDays=0;
+let empDailyHrsMap=new Map();
 let empDailyWageArr= new Array();
-let empDailyWageMap= new Map();
 
-function calcDailyWage(empHrs)
-{
-	return empHrs*WAGE_PER_HOUR;
+const findTotal=(totalVal,dailyVal)=>{
+	return totalVal+dailyVal;
 }
+let count =0;
+let totalHours=Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+let totalSalary=empDailyWageArr.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
 
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-{
-	totalWorkingDays++;
-	let empCheck=Math.floor(Math.random()*10)%3;
-	let empHrs= getWorkingHours(empCheck);
-	totalEmpHrs+=empHrs;
-	empDailyWageArr.push(calcDailyWage(empHrs));
-	empDailyWageMap.set(totalWorkingDays,calcDailyWage(empHrs));
+console.log("UC9A- Emp Wage with Arrow:"+"TotalHours:"+totalHours+"Total Wages:"+totalSalary);
 
-}
-console.log(empDailyWageMap);
-function totalWages(totalWage,dailyWage)
-{
-	return totalWage+dailyWage;
-}
-
-console.log("UC7A- Emp Wage Map totalHrs:"+Array.from(empDailyWageMap.values()).reduce(totalWages,0));
+let nonWorkingDays=new Array();
+let partWorkingDays=new Array();
+let fullWorkingDays=new Array();
+empDailyHrsMap.forEach((value,key,map)=>{
+	if(value==8) fullWorkingDays.push(key);
+	else if(value==4) partWorkingDays.push(key);
+	else nonWorkingDays.push(key);
+});
+console.log("Full Working Days:"+fullWorkingDays);
+console.log("Part Working Days:"+partWorkingDays);
+console.log("Non Working Days:"+nonWorkingDays);
